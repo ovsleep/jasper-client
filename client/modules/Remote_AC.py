8fe17ea2e45426cb392308dd1dfea24f2d165a04
guillermo <ovsleep@gmail.com>
2016-12-28 16:49:18 -0300

    ac response

--- client/modules/Remote_AC.py
+++ client/modules/Remote_AC.py
@@ -7,21 +7,29 @@ WORDS = ["AIRE, CALIENTE, FRIO, APAGAR"]
 
 def handle(text, mic, profile):
 	url = "http://192.168.1.101:9589/api/remote"
-	
+	response = 'Muy bien, '
+
 	if bool(re.search(r'\bapagar\b', text, re.IGNORECASE)):
 		mode =  'off'
 		action = { 'command': 'ac', 'data' : {'mode': mode} }
+		response += 'apago el aire'
+
 	else:
 		mic.say('qué temperatura?')
 		temp = mic.activeListen()
 		
 		if bool(re.search(r'\bcaliente\b', text, re.IGNORECASE)):
 			mode =  'hot'
+			response += 'aire caliente a '		
 		else:
 			mode = 'cold'
+			response += 'aire frío a '
 		
+		response += temp + ' grados.'
+
 		action = { 'command': 'ac', 'data' : {'mode': mode, 'temp': temp} }
 	requests.post(url, json=action)
+	mic.say(response)
 		
 def isValid(text):
 	return bool(re.search(r'\baire\b', text, re.IGNORECASE))
