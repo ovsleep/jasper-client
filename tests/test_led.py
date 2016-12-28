c9531a96f9bcdf6f76841109815a5389fb04952f
guillermo <ovsleep@gmail.com>
2016-12-28 14:15:53 -0300

    d

--- tests/test_led.py
+++ tests/test_led.py
@@ -4,24 +4,20 @@ import time
 
 class TestLed(unittest.TestCase):
 
-    @staticmethod
-    def getLed(self):
-        return led.Led()
-
     def testRed(self):
-        led = getLed()
+        led = led.Led()
         led.switch(True, 'RED')
         time.sleep(3)
         led.switch(False, 'RED')
 
     def testBlue(self):
-        led = getLed()
+        led = led.Led()
         led.switch(True, 'BLUE')
         time.sleep(3)
         led.switch(False, 'BLUE')
 
     def testGreen(self):
-        led = getLed()
+        led = led.Led()
         led.switch(True, 'GREEN')
         time.sleep(3)
         led.switch(False, 'GREEN')
