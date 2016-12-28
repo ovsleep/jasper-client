0958cd333dfcc2189af7fb1ea15b260d4a5194fa
guillermo <ovsleep@gmail.com>
2016-12-28 17:05:04 -0300

    dfs

--- client/modules/Remote_Off.py
+++ client/modules/Remote_Off.py
@@ -8,19 +8,25 @@ WORDS = ["APAGAR, TELE, AIRE, TODO, CHAU"]
 def handle(text, mic, profile):
 	url = "http://192.168.1.101:9589/api/remote"
 	device = ''
+	response = 'Muy bien, '
 
 	if bool(re.search(r'\btele\b', text, re.IGNORECASE)):
+		response += 'apago la tele'
 		device =  'tv'
 	if bool(re.search(r'\baire\b', text, re.IGNORECASE)):
 		device =  'ac'
+		response += 'apago el aire'
 	if bool(re.search(r'\btodo\b', text, re.IGNORECASE)):
+    	response += 'adios!'
 		device =  'all'
 	if bool(re.search(r'\bchau\b', text, re.IGNORECASE)):
+    	response += 'adios!'
 		device = 'all'
 		
 	action = { 'command': 'off', 'data' : {'device': device} }
 		
 	requests.post(url, json=action)
-		
+	mic.say(response)
+	
 def isValid(text):
 	return bool(re.search(r'\bapagar\b', text, re.IGNORECASE)) or bool(re.search(r'\bchau\b', text, re.IGNORECASE))
