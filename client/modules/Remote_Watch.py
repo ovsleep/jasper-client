4d4afad7450b39d46ee68e258d67fb54e2f613c2
guillermo <ovsleep@gmail.com>
2016-12-28 17:31:01 -0300

    sdf

--- client/modules/Remote_Watch.py
+++ client/modules/Remote_Watch.py
@@ -7,21 +7,27 @@ WORDS = ["VER, PELICULAS, TELE, CABLE, XBOX, CHROME"]
 
 def handle(text, mic, profile):
 	url = "http://192.168.1.101:9589/api/remote"
-	
+	response = 'Ok, '
 	device = ''
 	if bool(re.search(r'\bpeliculas\b', text, re.IGNORECASE)):
 		device = 'pi'
+		response += ' prendo la PI'
 	if bool(re.search(r'\btele\b', text, re.IGNORECASE)):
 		device =  'tv'
+		response += ' pongo la TV'
 	if bool(re.search(r'\bcable\b', text, re.IGNORECASE)):
 		device =  'cable'
+		response += ' prendo el cable'		
 	if bool(re.search(r'\bxbox\b', text, re.IGNORECASE)):
 		device =  'xbox'
+		response += ' pongo la x box'				
 	if bool(re.search(r'\bchrome\b', text, re.IGNORECASE)):
 		device =  'chrome'
-
+		response += ' pongo el crom cast'		
+		
 	action = { 'command': 'watch', 'data' : {'device': device} }
 	requests.post(url, json=action)
-		
+	mic.say(response)
+	
 def isValid(text):
 	return bool(re.search(r'\bver\b', text, re.IGNORECASE))
